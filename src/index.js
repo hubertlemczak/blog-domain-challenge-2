11a53b21425e293e3a1cb381826ed7e6f3ee5b2b
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-01 13:35:25 +0100

    build: init express server setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 4040;
+
+app.disable('x-powered-by');
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const postsRouter = require('./routers/posts');
+const categoriesRouter = require('./routers/categories');
+const usersRouter = require('./routers/users');
+
+app.use('/posts', postsRouter);
+app.use('/categories', categoriesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`\n[SERVER] Running on http://localhost:${port}\n`);
+});
