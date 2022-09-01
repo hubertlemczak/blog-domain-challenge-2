60d74dc8462374891bcf5fd0174d702a82569095
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-01 12:16:20 +0100

    feat: comment relation model with seed

---
+++ prisma/migrations/20220901105850_comment_relation/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "pictureUrl" SET DEFAULT 'default.pic/pic';
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "parentId" INTEGER,
+    "content" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,11 +3,53 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const createdUser = await prisma.user.create();
+  const createdUser = await prisma.user.create({
+    data: {
+      username: 'berto',
+      email: 'hubert@gmail.com',
+      password: 'pass123321',
+      profile: {
+        create: {
+          firstName: 'Hubert',
+          lastName: 'Lemczak',
+          age: 21,
+          pictureUrl: 'somepic.com',
+        },
+      },
+      comments: {
+        create: [
+          {
+            content: 'my parent comment',
+          },
+        ],
+      },
+    },
+    include: {
+      profile: true,
+      comments: true,
+    },
+  });
 
-  const allUsers = await prisma.user.findMany();
+  const createdComment = await prisma.comment.create({
+    data: {
+      content: 'my second parent comment',
+      userId: createdUser.id,
+      replies: {
+        create: [{ content: 'reply', userId: createdUser.id }],
+      },
+    },
+    include: { replies: true },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      comments: true,
+    },
+  });
+
   console.log(createdUser);
-  console.log(allUsers);
+  console.log(createdComment);
+  console.log(JSON.stringify(allUsers, null, 2));
 }
 
 main()
